86092477a467ed008744077a7865a2a8abd15070
alex-treebeard <alex@treebeard.io>
2024-02-28 17:14:36 +0000

    refactor main

--- kf_apps.tf
+++ kf_apps.tf
@@ -86,3 +86,33 @@ module "volumes_web_app" {
   ]
 }
 
+data "kustomization_overlay" "kubeflow_profile" {
+  resources = [
+    "${path.module}/overlays/profile"
+  ]
+
+  patches {
+    target {
+      kind = "Profile"
+      name = "prod"
+    }
+    patch = <<EOF
+apiVersion: kubeflow.org/v1
+kind: Profile
+metadata:
+  name: prod
+spec:
+  owner:
+    kind: User
+    name: user@example.com
+EOF
+  }
+}
+
+module "kubeflow_profile" {
+  source = "./modules/kust"
+  build  = data.kustomization_overlay.kubeflow_profile
+  depends_on = [
+    module.profiles_kfam
+  ]
+}
