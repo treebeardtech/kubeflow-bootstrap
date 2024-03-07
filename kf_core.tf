8f191c1aaf742c85ac053030ad27dcb192ddecb7
alex-treebeard <alex@treebeard.io>
2024-03-07 09:34:40 +0000

    bridge pattern

--- kf_core.tf
+++ kf_core.tf
@@ -8,100 +8,100 @@ resource "null_resource" "kf_core_start" {
   ]
 }
 
-resource "helm_release" "dex" {
-  name          = "dex"
-  namespace     = "argo-cd"
-  chart         = "${path.module}/charts/argo_app"
-  wait_for_jobs = true
-  values = [
-    <<EOF
-    name: dex
-    repoURL: https://github.com/kubeflow/manifests
-    path: common/dex/overlays/istio
-    targetRevision: 776d4f4
-    password: ${bcrypt(var.user_password)}
-    EOF
-  ]
-  depends_on = [
-    null_resource.kf_core_start,
-  ]
-}
+# resource "helm_release" "dex" {
+#   name          = "dex"
+#   namespace     = "argo-cd"
+#   chart         = "${path.module}/charts/argo_app"
+#   wait_for_jobs = true
+#   values = [
+#     <<EOF
+#     name: dex
+#     repoURL: https://github.com/kubeflow/manifests
+#     path: common/dex/overlays/istio
+#     targetRevision: 776d4f4
+#     password: ${bcrypt(var.user_password)}
+#     EOF
+#   ]
+#   depends_on = [
+#     null_resource.kf_core_start,
+#   ]
+# }
 
-resource "helm_release" "oidc_authservice" {
-  name          = "oidc-authservice"
-  namespace     = "argo-cd"
-  chart         = "${path.module}/charts/argo_app"
-  wait_for_jobs = true
-  values = [
-    <<EOF
-    name: oidc-authservice
-    repoURL: https://github.com/kubeflow/manifests
-    path: common/oidc-client/oidc-authservice/base
-    targetRevision: 776d4f4
-    EOF
-  ]
-  depends_on = [
-    helm_release.dex
-  ]
-}
+# resource "helm_release" "oidc_authservice" {
+#   name          = "oidc-authservice"
+#   namespace     = "argo-cd"
+#   chart         = "${path.module}/charts/argo_app"
+#   wait_for_jobs = true
+#   values = [
+#     <<EOF
+#     name: oidc-authservice
+#     repoURL: https://github.com/kubeflow/manifests
+#     path: common/oidc-client/oidc-authservice/base
+#     targetRevision: 776d4f4
+#     EOF
+#   ]
+#   depends_on = [
+#     helm_release.dex
+#   ]
+# }
 
-resource "helm_release" "kubeflow_namespace" {
-  name          = "kubeflow-namespace"
-  namespace     = "argo-cd"
-  chart         = "${path.module}/charts/argo_app"
-  wait_for_jobs = true
-  values = [
-    <<EOF
-    name: kubeflow-namespace
-    repoURL: https://github.com/kubeflow/manifests
-    path: common/kubeflow-namespace/base
-    targetRevision: 776d4f4
-    EOF
-  ]
-  depends_on = [
-    null_resource.kf_core_start
-  ]
-}
+# resource "helm_release" "kubeflow_namespace" {
+#   name          = "kubeflow-namespace"
+#   namespace     = "argo-cd"
+#   chart         = "${path.module}/charts/argo_app"
+#   wait_for_jobs = true
+#   values = [
+#     <<EOF
+#     name: kubeflow-namespace
+#     repoURL: https://github.com/kubeflow/manifests
+#     path: common/kubeflow-namespace/base
+#     targetRevision: 776d4f4
+#     EOF
+#   ]
+#   depends_on = [
+#     null_resource.kf_core_start
+#   ]
+# }
 
-resource "helm_release" "kubeflow_roles" {
-  name          = "kubeflow-roles"
-  namespace     = "argo-cd"
-  chart         = "${path.module}/charts/argo_app"
-  wait_for_jobs = true
-  values = [
-    <<EOF
-    name: kubeflow-roles
-    repoURL: https://github.com/kubeflow/manifests
-    path: common/kubeflow-roles/base
-    targetRevision: 776d4f4
-    EOF
-  ]
-  depends_on = [
-    helm_release.kubeflow_namespace
-  ]
-}
+# resource "helm_release" "kubeflow_roles" {
+#   name          = "kubeflow-roles"
+#   namespace     = "argo-cd"
+#   chart         = "${path.module}/charts/argo_app"
+#   wait_for_jobs = true
+#   values = [
+#     <<EOF
+#     name: kubeflow-roles
+#     repoURL: https://github.com/kubeflow/manifests
+#     path: common/kubeflow-roles/base
+#     targetRevision: 776d4f4
+#     EOF
+#   ]
+#   depends_on = [
+#     helm_release.kubeflow_namespace
+#   ]
+# }
 
-resource "helm_release" "kubeflow_istio_resources" {
-  count         = var.enable_istio_resources ? 1 : 0
-  name          = "kubeflow-istio-resources"
-  namespace     = "argo-cd"
-  chart         = "${path.module}/charts/argo_app"
-  wait_for_jobs = true
-  values = [
-    <<EOF
-    name: kubeflow-istio-resources
-    repoURL: https://github.com/kubeflow/manifests
-    path: common/istio-1-17/kubeflow-istio-resources/base
-    targetRevision: 776d4f4
-    hostname: '${var.hostname}'
-    enableHttps: '${var.enable_https}'
-    issuerName: '${var.issuer_name}'
-    EOF
-  ]
-  depends_on = [
-    helm_release.kubeflow_namespace
-  ]
-}
+# resource "helm_release" "kubeflow_istio_resources" {
+#   count         = var.enable_istio_resources ? 1 : 0
+#   name          = "kubeflow-istio-resources"
+#   namespace     = "argo-cd"
+#   chart         = "${path.module}/charts/argo_app"
+#   wait_for_jobs = true
+#   values = [
+#     <<EOF
+#     name: kubeflow-istio-resources
+#     repoURL: https://github.com/kubeflow/manifests
+#     path: common/istio-1-17/kubeflow-istio-resources/base
+#     targetRevision: 776d4f4
+#     hostname: '${var.hostname}'
+#     enableHttps: '${var.enable_https}'
+#     issuerName: '${var.issuer_name}'
+#     EOF
+#   ]
+#   depends_on = [
+#     helm_release.kubeflow_namespace
+#   ]
+# }
 
 resource "null_resource" "kf_core_end" {
   provisioner "local-exec" {
@@ -109,9 +109,9 @@ resource "null_resource" "kf_core_end" {
   }
 
   depends_on = [
-    helm_release.kubeflow_namespace,
-    helm_release.kubeflow_roles,
-    helm_release.kubeflow_istio_resources,
-    helm_release.oidc_authservice,
+    # helm_release.kubeflow_namespace,
+    # helm_release.kubeflow_roles,
+    # helm_release.kubeflow_istio_resources,
+    # helm_release.oidc_authservice,
   ]
 }
--- kf_dependencies.tf
+++ kf_dependencies.tf
@@ -5,109 +5,109 @@ resource "null_resource" "kf_dependencies_start" {
   }
 }
 
-resource "helm_release" "cert_manager" {
-  count            = var.enable_cert_manager ? 1 : 0
-  name             = "cert-manager"
-  namespace        = "cert-manager"
-  chart            = "cert-manager"
-  repository       = "https://charts.jetstack.io"
-  version          = "1.14.3"
-  create_namespace = true
-  depends_on = [
-    null_resource.kf_dependencies_start
-  ]
-  values = [
-    <<EOF
-    installCRDs: true
-    EOF
-  ]
-}
+# resource "helm_release" "cert_manager" {
+#   count            = var.enable_cert_manager ? 1 : 0
+#   name             = "cert-manager"
+#   namespace        = "cert-manager"
+#   chart            = "cert-manager"
+#   repository       = "https://charts.jetstack.io"
+#   version          = "1.14.3"
+#   create_namespace = true
+#   depends_on = [
+#     null_resource.kf_dependencies_start
+#   ]
+#   values = [
+#     <<EOF
+#     installCRDs: true
+#     EOF
+#   ]
+# }
 
-resource "helm_release" "istio_base" {
-  count            = var.enable_istio_base ? 1 : 0
-  name             = "istio-base"
-  namespace        = "istio-system"
-  chart            = "base"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  version          = "1.18.7"
-  create_namespace = true
-  depends_on = [
-    helm_release.cert_manager
-  ]
-  values = [
-    <<EOF
-    EOF
-  ]
-}
+# resource "helm_release" "istio_base" {
+#   count            = var.enable_istio_base ? 1 : 0
+#   name             = "istio-base"
+#   namespace        = "istio-system"
+#   chart            = "base"
+#   repository       = "https://istio-release.storage.googleapis.com/charts"
+#   version          = "1.18.7"
+#   create_namespace = true
+#   depends_on = [
+#     helm_release.cert_manager
+#   ]
+#   values = [
+#     <<EOF
+#     EOF
+#   ]
+# }
 
-resource "helm_release" "istiod" {
-  count            = var.enable_istiod ? 1 : 0
-  name             = "istiod"
-  namespace        = "istio-system"
-  chart            = "istiod"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  version          = "1.18.7"
-  create_namespace = true
-  depends_on = [
-    helm_release.istio_base
-  ]
-  values = [
-    <<EOF
-pilot:
-  resources:
-    requests:
-      cpu: 10m
-      memory: 128Mi
-global:
-  proxy:
-    resources:
-      requests:
-        cpu: 10m
-        memory: 64Mi
-    EOF
-  ]
-}
+# resource "helm_release" "istiod" {
+#   count            = var.enable_istiod ? 1 : 0
+#   name             = "istiod"
+#   namespace        = "istio-system"
+#   chart            = "istiod"
+#   repository       = "https://istio-release.storage.googleapis.com/charts"
+#   version          = "1.18.7"
+#   create_namespace = true
+#   depends_on = [
+#     helm_release.istio_base
+#   ]
+#   values = [
+#     <<EOF
+# pilot:
+#   resources:
+#     requests:
+#       cpu: 10m
+#       memory: 128Mi
+# global:
+#   proxy:
+#     resources:
+#       requests:
+#         cpu: 10m
+#         memory: 64Mi
+#     EOF
+#   ]
+# }
 
-resource "helm_release" "cluster_issuer" {
-  name      = "kubeflow-profile"
-  namespace = "cert-manager"
-  chart     = "${path.module}/charts/issuer"
-  values = [
-    <<EOF
-EOF
-  ]
-  depends_on = [
-    helm_release.cert_manager
-  ]
-}
+# resource "helm_release" "cluster_issuer" {
+#   name      = "kubeflow-profile"
+#   namespace = "cert-manager"
+#   chart     = "${path.module}/charts/issuer"
+#   values = [
+#     <<EOF
+# EOF
+#   ]
+#   depends_on = [
+#     helm_release.cert_manager
+#   ]
+# }
 
-resource "helm_release" "istio_ingressgateway" {
-  count      = var.enable_istiod ? 1 : 0
-  name       = "istio-ingressgateway"
-  namespace  = "istio-system"
-  chart      = "gateway"
-  repository = "https://istio-release.storage.googleapis.com/charts"
-  version    = "1.18.7"
-  depends_on = [
-    helm_release.istiod,
-    helm_release.cluster_issuer
-  ]
-  values = [
-    <<EOF
-service:
-  type: ClusterIP
-serviceAccount:
-  name: istio-ingressgateway-service-account
-resources:
-  requests:
-    cpu: 10m
-    memory: 64Mi
-  limits:
-    cpu: 2000m
-    memory: 1024Mi
-    EOF
-  ]
-}
+# resource "helm_release" "istio_ingressgateway" {
+#   count      = var.enable_istiod ? 1 : 0
+#   name       = "istio-ingressgateway"
+#   namespace  = "istio-system"
+#   chart      = "gateway"
+#   repository = "https://istio-release.storage.googleapis.com/charts"
+#   version    = "1.18.7"
+#   depends_on = [
+#     helm_release.istiod,
+#     helm_release.cluster_issuer
+#   ]
+#   values = [
+#     <<EOF
+# service:
+#   type: ClusterIP
+# serviceAccount:
+#   name: istio-ingressgateway-service-account
+# resources:
+#   requests:
+#     cpu: 10m
+#     memory: 64Mi
+#   limits:
+#     cpu: 2000m
+#     memory: 1024Mi
+#     EOF
+#   ]
+# }
 
 resource "helm_release" "argo_cd" {
   count = var.enable_argocd ? 1 : 0
@@ -131,8 +131,8 @@ EOF
 
 resource "time_sleep" "wait" {
   depends_on = [
-    helm_release.istiod,
-    helm_release.argo_cd,
+    # helm_release.istiod,
+    # helm_release.argo_cd,
   ]
 
   create_duration  = "10s"
@@ -153,11 +153,11 @@ resource "null_resource" "kf_dependencies_end" {
 
   depends_on = [
     time_sleep.wait,
-    helm_release.cert_manager,
-    helm_release.istio_base,
-    helm_release.istiod,
-    helm_release.istio_ingressgateway,
+    # helm_release.cert_manager,
+    # helm_release.istio_base,
+    # helm_release.istiod,
+    # helm_release.istio_ingressgateway,
     helm_release.argo_cd,
-    helm_release.cluster_issuer
+    # helm_release.cluster_issuer
   ]
 }
